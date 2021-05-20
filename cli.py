74ab5e329392f06a1622fd76df12b8666531879a
moahmed2104 <moahmed2104@gmail.com>
2021-05-20 11:44:14 -0600

    i will quickly start flask

--- cli.py
+++ cli.py
@@ -1,14 +1,60 @@
 import click
 import os
 
-__author__ = "Youssef El Mays"
+"""
+TODO
+    generate --> create new template, module, layout, stylesheet, controller
+    new --> set up new project
+    add --> add new dependency to virtualenv
+    run --> run flask app
+    serve --> run with GUNICORN
+"""
+
+__authors__ = ["Mohamed Ahmed", "Youssef El Mays"]
+
+@click.group()
+def cli():
+    pass
 
 @click.command()
+@click.option('-t', '--template', 'filetype', flag_value="template")
+@click.option('-m', '--module', 'filetype', flag_value="module")
+@click.option('-s', '--stylesheet', 'filetype', flag_value="stylesheet")
+@click.option('-c', '--controller', 'filetype', flag_value="controller")
 @click.argument("name", required=True)
-@click.option()
-def main(name):
-    click.echo("{}".format(name))
-    print("hello")
+def generate(name, filetype):
+    print(name, filetype)
+
+@click.command()
+@click.argument("modulename", required=True)
+def add(modulename):
+    print(modulename)
+
+@click.command()
+@click.option('-s', '--flask-session', prompt=True, type=bool, default=True)
+@click.option('-m', '--flask-mail', prompt=True, type=bool, default=True)
+@click.option('-b', '--bootstrap', prompt=True, type=bool, default=True)
+@click.option('-f', '--font-awesome', prompt=True, type=bool, default=True)
+@click.argument("name", required=True)
+def new(name, bootstrap, font_awesome, flask_session, flask_mail):
+    print(name, bootstrap, font_awesome)
+
+
+@click.command()
+@click.option('-p', "--port", type=int)
+@click.option('-h', "--host")
+def run(port, host):
+    print(port, host)
+
+@click.command()
+def serve():
+    click.echo()
+
+
+cli.add_command(generate)
+cli.add_command(add)
+cli.add_command(new)
+cli.add_command(run)
 
 if __name__ == "__main__":
-    main()
+    cli()
